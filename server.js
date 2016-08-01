32ac6d5e75cc0372911bce7d13db3122e12b4a30
DejonGill <dejonjgill@gmail.com>
2016-07-31 17:18:07 -0700

    Changes to server for deployment to heroku

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const express = require('express');
 const httpProxy = require('http-proxy');
 
 const isProduction = process.env.NODE_ENV === 'production';
-const port = isProduction ? process.env.PORT : 8500;
+
 
 const proxy = httpProxy.createProxyServer({
   changeOrigin: true
 });
 const app = express();
+
+app.set('port', (process.env.PORT || 8500));
 app.use(express.static('public'));
 const publicPath = path.resolve(__dirname, 'public');
 
@@ -20,8 +22,8 @@ app.get('/', function(req, res) {
   res.sendFile(`${publicPath}/index.html`);
 });
 
-var server = app.listen(port, function() {
-  console.log(`app listening on port ${port}`);
+var server = app.listen(app.get('port'), function() {
+  console.log(`app listening on port ${app.get('port')}`);
 });
 
 module.exports = server;
