267f714913dd111d0229712ccdfe3b76e1d984b4
DejonGill <dejonjgill@gmail.com>
2016-07-31 17:42:04 -0700

    node environment changed for heroku deploy

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const publicPath = path.resolve(__dirname, 'public');
 //   changeOrigin: true
 // });
 const app = express();
-
-app.set('port', (process.env.PORT || 8500));
+const port = isProduction ? process.env.PORT : 8500;
+// app.set('port', (process.env.PORT || 8500));
 app.use(express.static(publicPath));
 
 
@@ -22,8 +22,8 @@ app.get('/', function(req, res) {
   res.sendFile(`${publicPath}/index.html`);
 });
 
-var server = app.listen(app.get('port'), function() {
-  console.log(`app listening on port ${app.get('port')}`);
+var server = app.listen(port, function() {
+  console.log(`app listening on port ${port}`);
 });
 
 module.exports = server;
