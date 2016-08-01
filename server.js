d95794dfef46f31e33950536ab8682a17355962d
DejonGill <dejonjgill@gmail.com>
2016-08-01 15:16:50 -0700

    Removed Elm stuff

--- server.js
+++ server.js
@@ -8,15 +8,16 @@ const httpProxy = require('http-proxy');
 const isProduction = process.env.NODE_ENV === 'production';
 const publicPath = path.resolve(__dirname, 'public');
 
+
 // const proxy = httpProxy.createProxyServer({
 //   changeOrigin: true
 // });
 const app = express();
 const port = isProduction ? process.env.PORT : 8500;
-// app.set('port', (process.env.PORT || 8500));
-// app.use(publicPath);
-app.use(express.static(publicPath));
 
+app.use(express.static(publicPath));
+// allows bower components to be used in dev
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 
 app.get('/', function(req, res) {
