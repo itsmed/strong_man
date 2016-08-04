c2c5c5850f56a77ea9e5c1164795f62605122f53
DejonGill <dejonjgill@gmail.com>
2016-08-03 21:25:16 -0700

    Webpacking correctly

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+
+render(
+  <App />,
+  document.getElementById('app')
+);
